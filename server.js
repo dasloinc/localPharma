2d304383fd9a19388cb6eb6283f262955bedc333
Daniel Slobodscoy <danielslobodscoy@gmail.com>
2020-05-09 11:39:02 -0400

    fix routes
    fix routes

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-"use strict";
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+const app = express();
 
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
+var config = require(__dirname + "/config/config.json")[env];
 var db = {};
+
+var htmlRoute = require('./routes/html-routes.js');
+var postapiRoute = require('./routes/post-api-routes.js');
+var productsapiRoute = require('./routes/products-api-routes.js');
+
+
+app.use('/', htmlRoute);
+app.use('/', postapiRoute);
+app.use('/', productsapiRoute);
+app.use('./public', express.static(path.join(__dirname, 'public')))
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
