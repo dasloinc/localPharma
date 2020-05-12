b80f46a667def5328a8078c9547563fe36aab931
natashagils <56641651+natashagils@users.noreply.github.com>
2020-05-12 18:29:29 -0400

    Update checkout.js

--- public/assets/js/checkout.js
+++ public/assets/js/checkout.js
@@ -1,9 +1,27 @@
 $(document).ready(function () {
-    $.get("/api/products", function (data) {
-      for (const [index, val] of data.entries()) {
-        let row = $('<div></div>').append($('<span></span>').text(index+1), $('<span></span>').text(val.name));
-        $('#insertProducts').append(row);
-        console.log(row);
-      }
-    });
+
+
+
+// Make a request for a user with a given ID
+  axios.get('http://localhost:8080/api/products')
+  .then(function (response) {
+    // handle success
+    console.log(response);
   })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+
+  // $.get("/api/products", function (data) {
+  //   for (const [index, val] of data.entries()) {
+  //     let row = $('<div></div>').append($('<span></span>').text(index+1), $('<span></span>').text(val.name));
+  //     $('#insertProducts').append(row);
+  //     console.log(row);
+  //   }
+  // });
+})
