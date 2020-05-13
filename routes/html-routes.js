830b2d6d045bd83528519ab9c7327b6e0a9df96a
dmitribouilov <59586265+dmitribouilov@users.noreply.github.com>
2020-05-12 20:27:19 -0400

    Update html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,15 +4,9 @@ var path = require("path");
 
 module.exports = function(app) {
 
-  // create account route loads createacct.html
-  router.get("/", function(req, res) {
-    console.log(__dirname)
-    res.sendFile(path.join(__dirname, "public/index.html"));
-  });
-
-  router.get("/createacct", function(req, res) {
-    console.log(__dirname)
-    res.sendFile(path.join(__dirname, "../public/createacct.html"));
+ 
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/homepage.html"));
   });
 
   app.get("/createacct", function(req, res) {
